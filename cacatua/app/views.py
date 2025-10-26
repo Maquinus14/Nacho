bd59057ec6f08cde5b2941f215d72320d64f33ca
Maquinus <david.fernandez@colegiomiralmonte.es>
2025-10-26 18:10:54 +0100

    funcinal el 5v2

--- cacatua/app/views.py
+++ cacatua/app/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
 from .models import Person
 
-def diversion(request):
-    return render(request, 'app/app.html')
-
 # Create your views here.
 
 from .models import Person
 
 def diversion(request):
     person_list = Person.objects.all()
-    return render(request, 'home.html', {'persons': person_list})
+    return render(request, 'app/home.html', {'persons': person_list})
 
 def divertido(request, slug):
     person = Person.objects.get(slug=slug)
-    return render(request, 'person.html', {'person': person})
+    return render(request, 'app/person.html', {'person': person})
