8fec282795179bfb02abd3a1c9657ab693d718e4
Maquinus <david.fernandez@colegiomiralmonte.es>
2025-11-05 13:09:09 +0100

    Tarea 8

--- cacatua/users/urls.py
+++ cacatua/users/urls.py
@@ -3,5 +3,7 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path ('register/', views.register_view, name='register'),
-    path ('', views.register_view, name='home')
+    path ('', views.register_view, name='home'),
+    path ('login/', views.login_view, name='login'),
+    path ('logout/', views.logout_user, name='logout'),
 ]
--- cacatua/users/views.py
+++ cacatua/users/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("users:home")
+            user = form.save()
+            login(request, user)
+            return redirect("/")
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, "users/login.html", {"form": form})
+
+def logout_user(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('users:login')
